5e547cc57756ed3831c1fc3d04cde9e4a24a9946
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2021-11-08 17:28:00 +0530

    Update loops.sol

--- contracts/loops.sol
+++ contracts/loops.sol
@@ -2,17 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract loops {
-    uint256[5] public arr = [1, 2, 3, 4, 5];
-    uint256[5] public arr1;
 
-    function createLoop() public {
-        for (uint256 i = 0; i < arr.length; i++) {
-            arr[i] *= 2;
-        }
-    }
-
-    function returnArray() public view returns (uint256[5] memory) {
-        //array is of reference type thats why memory
-        return arr;
-    }
 }
